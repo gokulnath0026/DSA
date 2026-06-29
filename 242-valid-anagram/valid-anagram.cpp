c3fcebfe53484ed33d08504dddbe5c3d8516bf30
gokulnath0026 <142526583+gokulnath0026@users.noreply.github.com>
2026-06-29 13:42:05 +0530

    Time: 4 ms (34.96%) | Memory: 9.7 MB (49.43%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()){
-            return false;
+        
+        unordered_map<char,int>mp;
+        for(char c:s){
+            mp[c]++;
         }
-        int freq[26]={0};
-
-        for(char c : s ){
-            freq[c-'a']++;
+        for(char c:t){
+            mp[c]--;
         }
-        for(char c : t){
-            freq[c-'a']--;
-        }
-
-        for(int i=0;i<26;i++){
-            if(freq[i] != 0){
+        for(auto it:mp){
+            if(it.second != 0){
                 return false;
             }
             
         }
         return true;
-    }
+    }   
 };
