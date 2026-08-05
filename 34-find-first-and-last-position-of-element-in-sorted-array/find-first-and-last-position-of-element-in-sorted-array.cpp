692aba265a2f39b1c2a76914da022498cc721503
gokulnath0026 <142526583+gokulnath0026@users.noreply.github.com>
2026-08-05 10:03:09 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (90.03%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -27,14 +27,16 @@ public:
         int ans=-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                ans=mid;
-                low=mid+1;
-            }else if(nums[mid]<target){
-                low=mid+1;
-            }else{
-                high=mid-1;
-            }
+           if(nums[mid]==target){
+            ans=mid;
+            low=mid+1;
+           }
+           else if(nums[mid]<target){
+            low=mid+1;
+           }
+           else{
+            high=mid-1;
+           }
         }
         return ans;
     }
