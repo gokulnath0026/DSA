364625bb6d936f14ccdc2bc473710553ae7a4faf
gokulnath0026 <142526583+gokulnath0026@users.noreply.github.com>
2026-08-15 18:46:16 +0530

    Time: 3 ms (9.07%) | Memory: 9 MB (37.98%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int count=0;
+        stack<char>st;
+       for(char ch:s){
+        if(ch=='('||ch=='{'||ch=='['){
+            st.push(ch);
+        }
+        else if(ch==')'||ch=='}'||ch==']'){
+            if(st.empty()){
+                return false;
+            }
+            if(ch==')' && st.top()!='('){
+                return false;
+            }
+            if(ch=='}' && st.top()!='{'){
+                return false;
+            }
+            if(ch==']' && st.top()!='['){
+                return false;
+            }
+            st.pop();
+        }
+       }
+       if(st.empty()){
+        return true;
+       }
+       return false; 
+    }
+};
